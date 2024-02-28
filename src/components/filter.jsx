8485d2f13956b0e972629e228fe27a9d0f554828
Nishan Soni <nishan.soni2@gmail.com>
2024-02-28 13:17:04 -0700

    initial filter item stuff

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -4,8 +4,10 @@ const Filter = () => {
   return (
     <div className="h-full flex flex-col items-center py-6">
         <div className="text-3xl text-center"><strong>Filter</strong></div>
-        <div id="filter-items" className="mt-10 flex-1 w-full flex-col justify-evenly p-3">
-            <FilterItem name={"Level"} options={[]}/>
+        <div id="filter-items" className="mt-10 flex-1 w-full flex-col p-3">
+            <FilterItem name={"Level"} options={["100", "200", "300", "400", "500", "600"]}/>
+            <FilterItem name={"Status"} options={["Complete", "Incomplete", "In Progress"]}/>
+            <FilterItem name={"Semester Taken"} options={["Fall", "Winter", "Summer", "Spring"]}/>
         </div>
         <button>
             Apply Filters
--- src/components/filteritem.jsx
+++ src/components/filteritem.jsx
@@ -1,8 +1,21 @@
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
 const FilterItem = (props) => {
   const { name, options } = props;
   return (
-    <div className="flex flex-col flex-1 px-6">
-      <div className="text-xl text-gray-400"><strong>{name}</strong></div>
+    <div className="flex flex-col flex-1 px-6 m-5">
+      <div className="text-xl text-gray-400">
+        <strong>{name}</strong>
+      </div>
+      <FormGroup>
+        <div className="flex flex-row flex-wrap">
+          {options.map((item, index) => {
+            return <FormControlLabel key={item} control={<Checkbox />} label={item} />;
+          })}
+        </div>
+      </FormGroup>
+      <div className=" border-b-2"></div>
     </div>
   );
 };
