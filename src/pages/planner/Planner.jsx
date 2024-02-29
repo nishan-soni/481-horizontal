e3acf24e5bb4f9d46eb4679632250290c7c570b7
jRaimuu <liamsarj4@gmail.com>
2024-02-29 00:17:28 -0700

    Added sample dummy user-data and setup context and useData hook across app

--- src/pages/planner/Planner.jsx
+++ src/pages/planner/Planner.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import TablePage from "../../components/tablepage";
+import { useData } from '../../DataProvider';
 
 const Planner = () => {
+
+  const { data } = useData();
+  useEffect(() => {
+    console.log(data);
+  }, [])
+
   return (
     <>
       <div className="w-full h-full flex flex-row">
