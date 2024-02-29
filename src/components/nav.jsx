8abefbb7036f82d55c926c216609e647829dfebd
jRaimuu <liamsarj4@gmail.com>
2024-02-29 15:16:40 -0700

    Added basic styling to dashboard

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom"
 import NavLink from "./navlink";
 
 // Nav bar content
-const Nav = () => { 
-    return (
-      <div className="min-h-full flex flex-col items-center py-6 px-3">
-        <img src={ucalgarylogo} alt = "logo" className=" max-w-full h-auto" width="70%"/>
-        <div className="flex-1 flex flex-col justify-evenly items-start">
-          <Link to="/"><NavLink>Overview</NavLink></Link>
-          <Link to="/major"><NavLink>Major Requirements</NavLink></Link>
-          <Link to="/minor"><NavLink>Minor Requirements</NavLink></Link>
-          <Link to="/concentration"><NavLink>Concentration Requirements</NavLink></Link>
-          <Link to="/planner"><NavLink>Degree Planner</NavLink></Link>
-        </div>
+const Nav = () => {
+  return (
+    <div className="min-h-full w-full flex flex-col items-center py-6 px-3">
+      <div className="flex flex-row items-center justify-center w-full h-20 gap-4">
+        <img src={ucalgarylogo} alt="logo" className="object-contain h-full" />
+        <p className="font-bold text-3xl">Degree Requirements</p>
       </div>
-    );
-  };
-  
-  export default Nav;
+      <div className="flex-1 flex flex-col justify-evenly items-start">
+        <Link to="/"><NavLink>Overview</NavLink></Link>
+        <Link to="/major"><NavLink>Major Requirements</NavLink></Link>
+        <Link to="/minor"><NavLink>Minor Requirements</NavLink></Link>
+        <Link to="/concentration"><NavLink>Concentration Requirements</NavLink></Link>
+        <Link to="/planner"><NavLink>Degree Planner</NavLink></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -6,13 +6,15 @@ import { formatData } from '../../Data/Utilities'
 
 const Overview = () => {
 
-  const [newData, setNewData] = useState(null);
   const { data } = useData();
+  const [newData, setNewData] = useState(null);
 
 
   useEffect(() => {
     if (data != null) {
       setNewData(formatData(data))
+      // setStudent(Object.keys(data)[0])
+
     }
 
     // console.log(newData);
@@ -21,11 +23,35 @@ const Overview = () => {
   return (
     <>
       <div className="w-full h-full flex flex-row">
-        <div className="w-full h-full">Overview</div>
+        <div className="flex flex-col w-full h-full p-14 gap-y-8">
+
+          {/* Greeting */}
+          <div className="flex flex-col justify-between w-full h-full bg-blue-300 rounded-3xl p-4">
+            <div className="flex flex-row justify-between w-full">
+              <p className="text-4xl font-bold">Talia Ferris</p>
+              <p className="text-xl font-semibold">Year 3</p>
+            </div>
+
+            <div>
+              <p className="text-xl font-semibold">Human Resources</p>
+              <p className="text-2xl font-semibold">Bachelor of Commerce</p>
+            </div>
+          </div>
+
+          <div className="w-full h-full">
+            <p className="text-3xl font-semibold my-5">Winter</p>
+            <div className="w-full h-full lg:h-3/4 grid grid-cols-2 lg:grid-cols-5 rounded-3xl gap-2">
+              <div className="w-full h-full bg-yellow-200 border border-gray-300 hover:shadow-lg ease-in-out duration-300 rounded-2xl"> </div>
+              <div className="w-full h-full bg-yellow-200 border border-gray-300 hover:shadow-lg ease-in-out duration-300 rounded-2xl"> </div>
+              <div className="w-full h-full bg-yellow-200 border border-gray-300 hover:shadow-lg ease-in-out duration-300 rounded-2xl"> </div>
+              <div className="w-full h-full bg-yellow-200 border border-gray-300 hover:shadow-lg ease-in-out duration-300 rounded-2xl"> </div>
+              <div className="w-full h-full bg-yellow-200 border border-gray-300 hover:shadow-lg ease-in-out duration-300 rounded-2xl"> </div>
+            </div>
+          </div>
+
+        </div>
         <TablePage>
-          {
-            data != null && <PieChart data={newData} details={true} />
-          }
+          {data != null && <PieChart data={newData} details={true} />}
         </TablePage>
       </div>
     </>
