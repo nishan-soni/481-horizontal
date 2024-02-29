f3125d96e532d272c53b1183b1d811718a0df6e5
jRaimuu <liamsarj4@gmail.com>
2024-02-28 21:49:33 -0700

    Fixed styling issue with tablepage and made tablepage more modular in other components

--- src/components/tablepage.jsx
+++ src/components/tablepage.jsx
@@ -1,11 +1,12 @@
-import Filter from "./filter";
+import { ResponsivePie } from '@nivo/pie'
+// import Filter from "./filter"
 
-const TablePage = () => {
+const TablePage = ({children}) => {
   return (
-    <div className="flex flex-row h-full w-full justify-end">
-        <div className="w-[28%] border-l-2">
-            <Filter/>
-        </div>
+    <div className="flex flex-row h-full w-[28%] justify-end shadow-md">
+      <div className="w-full border-l-2">
+        {children}
+      </div>
     </div>
   );
 };
--- src/pages/planner/Planner.jsx
+++ src/pages/planner/Planner.jsx
@@ -1,10 +1,14 @@
+import TablePage from "../../components/tablepage";
 
-const Planner = () => { 
-    return (
-      <div>
-        <div>Planner</div>
+const Planner = () => {
+  return (
+    <>
+      <div className="w-full h-full flex flex-row">
+        <div className="w-full h-full">Planner</div>
+        <TablePage />
       </div>
-    );
-  };
-  
-  export default Planner;
+    </>
+  );
+};
+
+export default Planner;
