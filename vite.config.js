0cc7d24a43d23655daf128d011f108abdaedb2dc
Nishan Soni <nishan.soni2@gmail.com>
2024-03-04 19:42:48 -0700

    change base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/481-horizontal/",
+  base: "https://nishan-soni.github.io/481-horizontal",
 })
