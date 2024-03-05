0e27c7c951bf8b2ef5fb4abb1eedd6483359f8d6
jRaimuu <liamsarj4@gmail.com>
2024-03-04 20:09:37 -0700

    Made sidebar and side meu fixed to viewport; made topbar always visible while scrolling

--- src/components/tablepage.jsx
+++ src/components/tablepage.jsx
@@ -4,7 +4,7 @@ import { ResponsivePie } from '@nivo/pie'
 const TablePage = ({children}) => {
   return (
     <div className="flex flex-row h-full w-[28%] justify-end">
-      <div className="w-full border-l border-gray-200">
+      <div className="w-full border-l border-gray-200 relative">
         {children}
       </div>
     </div>
--- src/pages/overview/Overview.jsx
+++ src/pages/overview/Overview.jsx
@@ -32,40 +32,51 @@ const Overview = () => {
   return (
     <>
       <div className="w-full h-full flex flex-row">
-        <div className="flex flex-col w-full h-full p-14 gap-y-8 rounded-r-none bg-stone-50 shadow-inner">
 
-          {/* Greeting */}
-          <div className="w-full h-full relative">
-            <img src={welcome} alt="" className="absolute inset-0 w-full h-full rounded-2xl object-cover shadow-lg" />
-            <div className="absolute border-1 border-gray-200 inset-0 flex flex-col justify-between backdrop-blur-sm rounded-2xl p-4 text-white">
-              <div className="flex flex-row justify-between">
-                <p className="text-4xl font-bold">Talia Ferris</p>
-                <p className="text-xl font-semibold">Year 3</p>
-              </div>
-              <div>
-                <p className="text-xl font-semibold">Human Resources</p>
-                <p className="text-2xl font-semibold">Bachelor of Commerce</p>
+        <div className="w-full h-full flex flex-col">
+
+          <div className="flex flex-col w-full h-full p-14 gap-y-8 rounded-r-none bg-stone-50 shadow-inner">
+
+            {/* Greeting */}
+            <div className="w-full h-full relative">
+              <img src={welcome} alt="" className="absolute inset-0 w-full h-full rounded-2xl object-cover shadow-lg" />
+              <div className="absolute border-1 border-gray-200 inset-0 flex flex-col justify-between backdrop-blur-sm rounded-2xl p-4 text-white">
+                <div className="flex flex-row justify-between">
+                  <p className="text-4xl font-bold">Talia Ferris</p>
+                  <p className="text-xl font-semibold">Year 3</p>
+                </div>
+                <div>
+                  <p className="text-xl font-semibold">Human Resources</p>
+                  <p className="text-2xl font-semibold">Bachelor of Commerce</p>
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Semester */}
-          <div className="w-full h-full">
-            <p className="text-3xl font-semibold my-5">Winter</p>
-            <div className="w-full h-full lg:h-3/4 grid grid-cols-2 lg:grid-cols-5 rounded-3xl gap-2">
-              <Card imgsrc={greekCard} course={"GRST 205"}/>
-              <Card imgsrc={acctCard} course={"ACCT 341"}/>
-              <Card imgsrc={codeCard} course={"CPSC 329"}/>
-              <Card imgsrc={stockCard} course={"OPMA 301"}/>
-              <Card imgsrc={buildingCard} course={"OPMA 401"}/>
+            {/* Semester */}
+            <div className="w-full h-full">
+              <p className="text-3xl font-semibold my-5">Winter</p>
+              <div className="w-full h-full lg:h-3/4 grid grid-cols-2 lg:grid-cols-5 rounded-3xl gap-2">
+                <Card imgsrc={greekCard} course={"GRST 205"} />
+                <Card imgsrc={acctCard} course={"ACCT 341"} />
+                <Card imgsrc={codeCard} course={"CPSC 329"} />
+                <Card imgsrc={stockCard} course={"OPMA 301"} />
+                <Card imgsrc={buildingCard} course={"OPMA 401"} />
+              </div>
             </div>
+
+
           </div>
 
+          <div className="w-full h-[1000px]">
+            <p className="text-3xl font-semibold my-5">Incomplete</p>
+            <div className="w-full  bg-green-300">Text</div>
+          </div>
         </div>
+
         <TablePage>
-          <div className="flex flex-col justify-between h-full bg-yellow-20">
-          {data != null && <PieChart data={newData} details={true} mb={"125"} />}
-          <Gpa/>
+          <div className="flex flex-col justify-between h-full w-full  max-w-[18%] top-0 fixed">
+            {data != null && <PieChart data={newData} details={true} mb={"125"} />}
+            <Gpa />
           </div>
         </TablePage>
       </div>
